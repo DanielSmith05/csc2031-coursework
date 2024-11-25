b509dab1fbe78f233ebf12754ea0748792626cf2
DanielSmith05 <d.smith24@newcastle.ac.uk>
2024-11-25 20:16:00 +0000

    exercise 3

--- config.py
+++ config.py
@@ -1,2 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
+
+# IMPORT BLUEPRINTS
+from accounts.views import accounts_bp
+from posts.views import posts_bp
+from security.views import security_bp
+
+# REGISTER BLUEPRINTS
+app.register_blueprint(accounts_bp)
+app.register_blueprint(posts_bp)
+app.register_blueprint(security_bp)
