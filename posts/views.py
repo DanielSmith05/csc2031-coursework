f7db97982a396b24eee144a58b31d85caefad36c
DanielSmith05 <d.smith24@newcastle.ac.uk>
2024-11-27 13:24:16 +0000

    excercise 5

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, url_for, redirect
+from config import db, Post
+from posts.forms import PostForm
 
 posts_bp = Blueprint('posts', __name__, template_folder='templates')
 
@@ -6,9 +8,21 @@ posts_bp = Blueprint('posts', __name__, template_folder='templates')
 def posts():
     return render_template('posts/posts.html')
 
-@posts_bp.route('/create')
+@posts_bp.route('/create', methods=('GET', 'POST'))
 def create():
-    return render_template('posts/create.html')
+
+    form = PostForm()
+
+    if form.validate_on_submit():
+        new_post = Post(title=form.title.data, body=form.body.data)
+
+        db.session.add(new_post)
+        db.session.commit()
+
+        flash('Post created', category='success')
+        return redirect(url_for('posts.posts'))
+
+    return render_template('posts/create.html', form=form)
 
 @posts_bp.route('/update')
 def update():
